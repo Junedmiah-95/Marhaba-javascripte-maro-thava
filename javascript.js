ce8d928cb300079712bb40a5c419c680273383ed
Juned Miah <201196603+Junedmiah-95@users.noreply.github.com>
2025-05-02 23:05:48 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,514 @@
+//--------------------------------------------
+                // lesson:02
+//--------------------------------------------
+
+//--------------------------------------------
+                //Solve page:35
+//--------------------------------------------
+/*
+// Practice:01 Solve
+const myBirthYear = 1995;
+console.log(myBirthYear);
+
+// Practice:02 Solve
+const sibiling = 'six';
+console.log(sibiling);
+
+// Practice:03 Solve
+const students = 100;
+console.log(students);
+
+// Practice:04 Solve
+const marks = "90%";
+console.log(marks);
+
+// Practice:05 Solve
+const rooms = '06';
+console.log(rooms);
+
+// Practice:06 Solve
+const studyTime = "around 3 hours" 
+console.log(studyTime)
+
+// Practice:07 Solve
+// this is my contract no 01753118854
+const total = 0+1+7+5+3+1+1+8+8+5+4;
+console.log(total);
+//----------------------------------------------
+                  //Solve page:38
+//----------------------------------------------
+
+// Practice:01 Solve
+const Age = 29;
+console.log(Age);
+
+// Practice:02 Solve
+const booksToRead = 10;
+console.log(booksToRead);
+
+// Practice:03 Solve
+const wakeUpTime = "06:00 am";
+console.log(wakeUpTime)
+
+// Practice:04 Solve
+const WastedTime = "around 5 hours";
+console.log(WastedTime)
+
+// Practice:05 Solve
+const borrow = "8000tk";
+console.log(borrow);
+
+// Practice:06 Solve
+const goals = "30 Goals";
+console.log(goals);
+
+// Practice:07 Solve
+const pillow = 01;
+console.log(pillow)
+
+
+//----------------------------------------------
+                  //Solve page:41
+//----------------------------------------------
+
+// Practice:01 Solve
+const name = "juned Miah";
+console.log(name);
+// name ar---- maje ai tan hoy ken?
+
+// Practice:02 Solve
+const address = "Aminpur, Tajpu-3123, Osmaninagar, Sylhet, Bangladesh.";
+console.log(address);
+// Practice:03 Solve
+const MyFriendSchoolName = "MD Mojnu Miah Academy";
+console.log(MyFriendSchoolName);
+
+// Practice:04 Solve
+const Fan = "I have no Fevourite Naika Boss";
+console.log(Fan);
+
+// Practice:05 Solve
+const movieOfTheeYear = "as if I have no Favourite Naika, That's why I have no Movie";
+console.log(movieOfTheeYear);
+
+// Practice:06 Solve
+const hobby = "Solve javascript problem";
+console.log(hobby);
+
+// Practice:07 Solve
+const color = " I have no Favourite color";
+console.log(color);
+
+//----------------------------------------------
+                  //Solve page:43
+//----------------------------------------------
+
+// Practice:01 Solve
+const MyDearFriendAreYouHappyForYourExamResult = false
+console.log(MyDearFriendAreYouHappyForYourExamResult);
+
+// Practice:02 Solve
+const IsMyCrushSentMeAnySmS = true;
+console.log(IsMyCrushSentMeAnySmS);
+
+// Practice:03 Solve
+const MyFavourieTeamIsWin = true;
+console.log(MyFavourieTeamIsWin);
+
+// Practice:04 Solve
+const IsHappy= true;
+const hasMoney = false;
+const isHungry = false;
+console.log(IsHappy,hasMoney,isHungry);
+
+// Practice:05 Solve
+const isStudyDone = false;
+console.log(isStudyDone);
+
+// Practice:06 Solve
+const isPhoneCharge = false;
+console.log(isPhoneCharge)
+
+// Practice:07 Solve
+const isDinnerDone = true;
+console.log(isDinnerDone);
+
+
+//----------------------------------------------
+                  //Solve page:47
+//----------------------------------------------
+
+// Practice:01 Solve
+const revenue = 18000;
+const spend = 15000;
+const Oboshishto = revenue - spend;
+console.log(Oboshishto)
+
+// Practice:02 Solve
+const iHavePencile = 10
+const perDayUse = 2
+const useTime = iHavePencile/perDayUse;
+console.log(useTime);
+
+// Practice:03 Solve
+const iHaveMoney = 60;
+const perTimeISpend = 15;
+const iCanSpend = iHaveMoney/perTimeISpend;
+const afterSpendIHaveMoney = iHaveMoney%perTimeISpend;
+console.log(iCanSpend);
+console.log(afterSpendIHaveMoney);
+
+// Practice:04 Solve
+const x = 'Hello';
+const y = 'World';
+const z = x+y;
+console.log(z)
+
+// Practice:05 Solve
+const Rice = 152;
+const dal = 261;
+const totalBuy = Rice + dal;
+console.log(totalBuy);
+
+// Practice:06 Solve
+const HaveMoney = 500;
+const perTimeIUse = 75;
+const SpendTime = HaveMoney/perTimeIUse;
+const afterSpendMoney = HaveMoney%perTimeIUse;
+console.log(SpendTime);
+console.log(afterSpendMoney);
+
+// Practice:07 Solve
+const X = 8;
+const Y = 3;
+const Z = X / Y;
+const W = X%Y;
+console.log(Z);
+console.log(W);
+
+// Practice:08 Solve
+const A = 50
+const B = 9;
+const C = A%B;
+console.log(C);
+
+// Practice:09 Solve
+const Amar = 'Bangla';
+const tumar = 'Desh';
+const AmerRtumar = Amar + tumar;
+console.log(AmerRtumar);
+
+// Practice:10 Solve
+const amraKaceCaulAce = 480;
+const amraPerDayNeed = 4;
+const amiKeteParbo = amraKaceCaulAce/amraPerDayNeed ; 
+console.log(amiKeteParbo);
+const AmiKeteParbo = 120;
+const amrajaniOneMonth = 30;
+const keteParbo = AmiKeteParbo/amrajaniOneMonth;
+console.log(keteParbo);
+
+//--------------------------------------------
+                // lesson:03
+//--------------------------------------------
+//--------------------------------------------
+                  //Solve page:50
+//--------------------------------------------
+
+// Practice:01 Solve
+const price = 555
+console.log(typeof price);
+
+// Practice:02 Solve
+const  learning = true;
+console.log (typeof learning);
+
+// Practice:03 Solve
+const megacity = 'Dhaka';
+console.log(typeof megacity);
+
+// Practice:04 Solve
+const temperature = 102.5;
+console.log(typeof temperature);
+
+// Practice:05 Solve
+const IsHungry = true;
+console.log(typeof IsHungry);
+
+// Practice:06 Solve
+const isRich = true;
+console.log(typeof isRich);
+// Practice:07 Solve
+
+//--------------------------------------------
+                  //Solve page:52
+//--------------------------------------------
+
+// Practice:01 Solve
+let age = 30;
+age = 29;
+console.log(age);
+
+// Practice:02 Solve
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+// Practice:03 Solve
+let StudyTime = 4;
+StudyTime = 0;
+console.log(StudyTime);
+
+// Practice:04 Solve
+let friendsCount = 3;
+friendsCount = 2;
+console.log(friendsCount);
+
+// Practice:05 Solve
+let phonePrice = 25000;
+console.log(phonePrice);
+phonePrice = 12000;
+console.log(phonePrice);
+phonePrice =10000;
+console.log(phonePrice);
+
+// Practice:06 Solve
+let chairCount = 40;
+console.log(chairCount);
+chairCount = 38
+console.log(chairCount);
+chairCount = 32
+console.log(chairCount);
+
+// Practice:07 Solve
+let playTime = "3 hours";
+console.log(playTime);
+playTime = "0 hours";
+console.log(playTime);
+/*
+//--------------------------------------------
+                  //Solve page:56
+//--------------------------------------------
+
+// Practice:01 Solve
+// aita argentina VS Braziler football khelar score
+const score = "07:00";
+console.log(score);
+
+// Practice:02 Solve
+// redius ki
+//ar mane to
+//ami janina
+const redius = "000";
+console.log(redius);
+*/
+
+/*
+// Practice:03 Solve
+
+/* ami job kori ai hisebe amar
+ 1 ta dress ace r sob dress mainsore 
+ dilaici karon amar e oto kapr okon pinder time nai*/
+
+ /*
+const  dress = 1 ;
+console.log(dress);
+
+// Practice:04 Solve
+/*amare taka gift korar moto kunu manus nai 
+tarpor o ami dorlam 100 taka paici*/
+
+/*
+const giftMoney = "100 Taka";
+console.log(giftMoney);
+
+/*
+
+// Practice:05 Solve
+
+/*1st ami bangla pormu
+2nd english
+3rd time math kormu
+4th time biggan prmu
+5th time islam shikka pormu*/
+
+/*
+const studytime = "7 hours";
+console.log(studytime);
+
+
+/*
+// Practice:06 Solve
+
+//ami kokono karu kac teke taka dar korina
+//kintu jodi keu bipode pore taka dar cay taile ami help kori
+// suhel bai k abong subo bai k 1000 1000 kore 2000 duijonke dar dici
+
+const sendMoney = "100 Taka";
+console.log(sendMoney);
+
+
+
+// Practice:07 Solve
+/* online teke ekon kunu kicu order korini*/
+/*
+const pay = "200 Taka";
+console.log(pay);
+*/
+/*
+//--------------------------------------------
+                  //Solve page:59
+//--------------------------------------------
+
+// Practice:01 Solve
+const favouriteFood = "fast food";
+console.log(favouriteFood);
+
+// Practice:02 Solve
+const city = "Chittagon";
+const City = "Dhaka";
+console.log(city);
+console.log(City);
+
+// Practice:03 Solve
+const userName ="Johan";
+const username = "Jovan";
+console.log(userName);
+console.log(username);
+
+// Practice:04 Solve
+const amarVasarTikana = "Aminapur";
+console.log(amarVasarTikana)
+
+// Practice:05 Solve
+// he aitate problem ase "super" word tite tai "Super" liklam 
+const Super ="36 Ran";
+console.log(Super);
+
+// Practice:06 Solve
+// he aitate problem ase "class" word tite tai "Class" liklam
+const Class = "compelate my graduation";
+console.log(Class);
+
+// Practice:07 Solve
+// aikane problem ase variable a 1st a noumber dya jabe na tai change
+// kore variable liksi
+//const 2ndPosition = "Mushfik";
+
+
+const secondPosition = "Mushfik";
+console.log(secondPosition);
+
+
+*/
+
+//--------------------------------------------
+                  //Solve page:
+//--------------------------------------------
+
+// Practice:01 Solve
+
+
+// Practice:02 Solve
+
+
+// Practice:03 Solve
+
+
+// Practice:04 Solve
+
+
+// Practice:05 Solve
+
+
+// Practice:06 Solve
+
+
+// Practice:07 Solve
+
+
+
+//--------------------------------------------
+                  //Solve page:
+//--------------------------------------------
+
+// Practice:01 Solve
+
+
+// Practice:02 Solve
+
+
+// Practice:03 Solve
+
+
+// Practice:04 Solve
+
+
+// Practice:05 Solve
+
+
+// Practice:06 Solve
+
+
+// Practice:07 Solve
+
+
+
+//--------------------------------------------
+                  //Solve page:
+//--------------------------------------------
+
+// Practice:01 Solve
+
+
+// Practice:02 Solve
+
+
+// Practice:03 Solve
+
+
+// Practice:04 Solve
+
+
+// Practice:05 Solve
+
+
+// Practice:06 Solve
+
+
+// Practice:07 Solve
+
+
+
+//--------------------------------------------
+                  //Solve page:
+//--------------------------------------------
+
+// Practice:01 Solve
+
+
+// Practice:02 Solve
+
+
+// Practice:03 Solve
+
+
+// Practice:04 Solve
+
+
+// Practice:05 Solve
+
+
+// Practice:06 Solve
+
+
+// Practice:07 Solve
+
+
+// Practice: Solve
+
+
+// Practice: Solve
+// Practice: Solve
+
+// Practice: Solve
